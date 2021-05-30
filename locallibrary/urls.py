c80361f278932dd1c46e64c53bc18abe7b5d5f04
mohamadhoseinmarvi <mohamad.hosein.marviy@gmail.com>
2021-05-29 19:53:56 -0700

    day4

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -25,3 +25,6 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
